409b8e50996d63c123d8ad3793e2d7bf15fdc72a
Ayush Nagar <ayushnagar0018@gmail.com>
2024-09-25 10:23:29 +0530

    Fruits Vegetable frontend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [file, setFile] = useState(null);
+  const [imagePreview, setImagePreview] = useState(null);
+  const [prediction, setPrediction] = useState(null);
+  const [loading,setLoading] = useState(false);
+
+  const handleFileChange = (e) => {
+    setFile(e.target.files[0]);
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setImagePreview(reader.result);
+      };
+      reader.readAsDataURL(file);
+      
+    }
+  };
+
+  const handleSubmit = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+
+    if (!file) {
+      alert("Please select a file.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+    console.log(formData);
+
+    try {
+      const response = await fetch("http://localhost:5000/predict", {
+        method: "POST",
+        body: formData,
+      });
+
+      const data = await response.json();
+      setPrediction(data.prediction)
+      setLoading(false);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+            <h1>Fruits🍍-Vegetable🍅 Classification</h1>
+            <form onSubmit={handleSubmit}>
+                <input type="file" accept="image/jpeg, image/png" onChange={handleFileChange} />
+                <button type="submit">Upload</button>
+            </form>
+            {loading && <div>Loading...</div>}
+            {/* {imagePreview && (
+                <div>
+                    <h2>Image Preview:</h2>
+                    <img
+                        src={imagePreview}
+                        alt="Preview"
+                        style={{ width: '250px', height: '250px' }}
+                    />
+                </div>
+            )} */}
+            {prediction && !loading && (
+                <div>
+
+                    <h3>Predicted: {prediction}</h3>
+                </div>
+            )}
+        </div>
   );
 }
 
